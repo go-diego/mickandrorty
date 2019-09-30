ed972a929ec5eb41968afc90417ea6b76806fcd7
Diego Bernal <diego91bernal@gmail.com>
2019-09-29 20:27:32 -0700

    feature: add react-helmet + og metatags

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import ogImg from "../static/thumbnail.png";
 
 interface Props {
   children: React.ReactNode;
@@ -9,6 +11,17 @@ interface Props {
 export default function Layout({ children }: Props) {
   return (
     <React.Fragment>
+      <Helmet>
+        <title>Mick & Rorty</title>
+        <meta property="og:url" content="https://mickandrorty.netlify.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Mick & Rorty" />
+        <meta
+          property="og:description"
+          content="An Unofficial Rick and Morty app"
+        />
+        <meta property="og:image" content={ogImg} />
+      </Helmet>
       <header>
         <Navbar />
       </header>
